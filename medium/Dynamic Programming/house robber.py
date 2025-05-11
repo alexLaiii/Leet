f96ccabbe00e460baac2816a0ed48c61a778f80f
Alex Lai <127812749+alexLaiii@users.noreply.github.com>
2025-05-11 19:53:45 -0400

    Create house robber.py

---
+++ medium/Dynamic Programming/house robber.py
@@ -0,0 +1,23 @@
+"""
+Classic dynamic programming problem
+Idea:
+-curr_rob always store the largest number you can rob at house[i]
+-Tricky initialization, the first house profit is the first house, the second house profit has to be the max(first house, second house)
+"""
+
+
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if(len(nums) <= 1): return nums[0]
+        else:
+            prev_rob, curr_rob = nums[0], max(nums[0], nums[1])
+            for i in range(2, len(nums)):
+                temp = curr_rob
+                curr_rob = max(prev_rob + nums[i], curr_rob)
+                prev_rob = temp
+                
+            return curr_rob
