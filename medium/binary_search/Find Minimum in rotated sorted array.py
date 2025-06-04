86215c9392a37c3b494864e8c5a17c9490a2a992
Alex Lai <127812749+alexLaiii@users.noreply.github.com>
2025-06-04 14:24:19 -0400

    Update Find Minimum in rotated sorted array.py

--- medium/binary_search/Find Minimum in rotated sorted array.py
+++ medium/binary_search/Find Minimum in rotated sorted array.py
@@ -1,11 +1,11 @@
-##
+"""
 Initialize two pointers, left and right.
 Use binary search to narrow down the region containing the minimum.
-If nums[mid] > nums[right], the minimum lies to the right.
-Otherwise, it lies to the left or could be the mid itself.
+If nums[mid] > nums[right], the minimum lies to the right. (Because it is rotated, a normal sorted array will never satisfy this condition)
+Otherwise, it lies to the left or could be the mid itself. (Might rotated, might not, but the rotation didnt cause minimum value lies in the right, tho there might be larger value lies in the left)
 The loop ends when left == right, pointing to the minimum.
 This is a clean and optimal binary search pattern that's highly valuable in technical interviews.
-##
+"""
 
 class Solution(object):
     def findMin(self, nums):
@@ -13,13 +13,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        left, right = 0, len(nums) - 1
-        while(left < right):
-            mid = (left + right) // 2
-            if(nums[mid] > nums[right]):
+        left = 0
+        right = len(nums) - 1
+        while right > left:
+            mid = (right + left) // 2
+            # if mid > right, then the array must be rotated, the minimum lies in the right direction
+            if nums[mid] > nums[right]:
                 left = mid + 1
-            if(nums[mid] < nums[right]):
-                if(nums[mid] < nums[mid - 1]):
-                    return nums[mid]
-                right = mid - 1
+            elif nums[mid] <= nums[right]:
+                right = mid
         return nums[left]
+
