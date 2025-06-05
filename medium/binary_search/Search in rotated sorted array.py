d87faf013284559b89874b19e71223a1b00585c5
Alex Lai <127812749+alexLaiii@users.noreply.github.com>
2025-06-05 16:10:53 -0400

    Update Search in rotated sorted array.py

--- medium/binary_search/Search in rotated sorted array.py
+++ medium/binary_search/Search in rotated sorted array.py
@@ -1,38 +1,29 @@
-## Approach:
-Find Pivot first
-Find the index
-##
+"""
+This problem is a fking hell
+"""
 
 
 class Solution(object):
     def search(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
-        left, right = 0, len(nums) - 1
-        while left < right:
-            mid = (left + right) // 2
-            if(nums[mid] > nums[right]):
-                left = mid + 1
-            if(nums[mid] < nums[right]):
-                if(nums[mid - 1] > nums[mid]):
-                    left = mid
-                    break
-                right = mid - 1
-
-        low = left
-        high = low + len(nums) - 1
-        while(low <= high):
-            mid = (low + high) // 2 - len(nums)
-            if(target == nums[mid]):
-                if(mid < 0):
-                    return mid + len(nums)
+        l, r = 0, len(nums) - 1
+        while r >= l:
+            m = (l+r) // 2
+            if nums[m] == target:
+                return m
+            if nums[m] >= nums[l]:
+                # must land on right portion, 
+                if target > nums[m] or target < nums[l]:
+                    l = m + 1
                 else:
-                    return mid 
-            if(target > nums[mid]):
-                low = mid + len(nums) + 1
+                    r = m - 1
             else:
-                high = mid + len(nums) - 1
+                if target < nums[m] or target > nums[r]:
+                    r = m - 1
+                else:
+                   l = m + 1
         return -1
+
+            
+     
+
+            
