109e89e3adce08853b72736b5ae7490a0ec031ad
Alex Lai <127812749+alexLaiii@users.noreply.github.com>
2025-06-21 22:47:08 -0400

    Update Number of Enclaves.py

--- medium/Graph/Number of Enclaves.py
+++ medium/Graph/Number of Enclaves.py
@@ -57,3 +57,45 @@ class Solution:
                 if grid[i][j] == 1 and (i,j) not in visited:
                     count += 1
         return count
+
+
+
+
+"""
+To save space for visited, I update each traveled cell to 0 (flood), so I dont need to maintain a visited set.
+Now, in the final loop, if anything is 1, it must be an enclaves.
+"""
+class Solution:
+    def numEnclaves(self, grid: List[List[int]]) -> int:
+        M, N = len(grid), len(grid[0])
+
+        directions = [[1,0], [0,-1],[-1,0],[0,1]]
+        def dfs(r,c):
+            if r < 0 or c < 0 or r >= M or c >= N or grid[r][c] == 0:
+                return
+            grid[r][c] = 0
+            for dr,dc in directions:
+                dfs(r+dr, c+dc)
+            
+      
+        for i in range(M):
+            if grid[i][0] == 1:
+                dfs(i,0)
+            if grid[i][N-1] == 1:
+                dfs(i, N-1)
+        
+        for j in range(N):
+            if grid[0][j] == 1:
+                dfs(0, j)
+            if grid[M-1][j] == 1:
+                dfs(M-1, j)
+        
+        enclaves = 0
+        for i in range(M):
+            for j in range(N):
+                if grid[i][j] == 1:
+                    enclaves += 1
+        return enclaves
+                
+            
+        
