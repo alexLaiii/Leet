d16d2373abe73cf0d95d12d716b3e83a9caced49
Alex Lai <127812749+alexLaiii@users.noreply.github.com>
2025-06-24 20:52:37 -0400

    Update Interleaving String.py

--- medium/(2D) Dynammic Programming/Interleaving String.py
+++ medium/(2D) Dynammic Programming/Interleaving String.py
@@ -34,9 +34,10 @@ If neither option is valid, this path is **invalid**, and no further build is po
 ---
 
 Time Complexity: O(m * n), where m = len(s2), n = len(s1) — full DP grid traversal  
-Space Complexity: O(m * n) — DP grid of size (m+1) × (n+1)
+Space Complexity(2D): O(m * n) — DP grid of size (m+1) × (n+1)
+Space Complexity(1D): O(n) - 1D DP with n = len(s2)
 """
-
+# 2D solution
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
         lenS1, lenS2 = len(s1),len(s2)
@@ -53,6 +54,39 @@ class Solution:
                 if i > 0 and dp[i - 1][j] and s2[i - 1] == s3[i + j - 1]:
                     dp[i][j] = True
         return dp[lenS2][lenS1]
+
+
+# Optimize 1D solution (Same Idea, optimize to only keep track of the previous row)
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        if len(s1) + len(s2) != len(s3):
+            return False
+        M, N = len(s1), len(s2)
+        dp = [False for i in range(N + 1)]
+        dp[0] = True
+
+        # Base case: fill in first row, only using s2
+        for j in range(1, N + 1):
+            if s2[j-1] == s3[j-1] and dp[j-1]:
+                dp[j] = True
+        
+        for i in range(1, M + 1):
+            new_row = [False for k in range(N + 1)]
+            # First column: only using s1
+            if s1[i - 1] == s3[i - 1] and dp[0]:
+                new_row[0] = True
+            for j in range(1, N + 1):
+                if s1[i - 1] == s3[i + j -1] and dp[j]:
+                    new_row[j] = True
+                if s2[j - 1] == s3[i + j -1] and new_row[j-1]:
+                    new_row[j] = True
+            dp = new_row
+        return dp[-1]
+        
+        
+
+
+
                 
             
 
