cc0b2df070b5b94e01e03679fa44f2a33eb7b91a
Alex Lai <127812749+alexLaiii@users.noreply.github.com>
2025-07-12 20:10:14 -0400

    Update Course Schedule II.py

--- medium/Graph/Course Schedule II.py
+++ medium/Graph/Course Schedule II.py
@@ -1,5 +1,20 @@
 """
-Topological Sort via DFS
+Use same idea from Course Schedule but track the path of course finishing.
+Topological Sort via DFS.
+
+Idea:
+We apply topological sorting to determine a valid order in which to take courses.
+
+- A course with no prerequisites can be taken immediately.
+- For each course, we perform a DFS to recursively visit all of its prerequisites.
+- We mark nodes as visited during DFS to detect cycles (i.e., circular dependencies).
+- After visiting all prerequisites of a course, we add the course to the result list.
+  This ensures that prerequisites are always added before the course that depends on them.
+- The graph is built such that each course maps to a list of its prerequisites (i.e., course → prereqs).
+
+Since we're adding courses post-recursively, the result is already in the correct topological order (no need to reverse).
+
+Time Complexity: O(V+E)
 """
 
 
@@ -16,6 +31,7 @@ class Solution:
             # Cyclic graph
             if course in visited:
                 return False
+            # This course has been checked and can be finished, return True
             if course in taken:
                 return True
             visited.add(course)
