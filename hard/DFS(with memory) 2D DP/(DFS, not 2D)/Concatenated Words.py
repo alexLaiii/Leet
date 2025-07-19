b8fb44ac60ad828cd469b43ad579fa5ac65ba49d
Alex Lai <127812749+alexLaiii@users.noreply.github.com>
2025-07-19 19:21:15 -0400

    Create Concatenated Words.py

---
+++ hard/DFS(with memory) 2D DP/(DFS, not 2D)/Concatenated Words.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findAllConcatenatedWordsInADict(self, words: List[str]) -> List[str]:
+        word_set = set(words)
+        
+        cache = {}
+        def dfs(word):
+            if word in cache:
+                return cache[word]
+            for  i in range(1, len(word)):
+                prefix = word[:i]
+                suffix = word[i:]
+                if (prefix in word_set and suffix in word_set) or (prefix in word_set and dfs(suffix)):
+                    cache[word] = True
+                    return True
+            cache[word] = False
+            return False
+        res = []
+        for w in words:
+            if dfs(w):
+                res.append(w)
+        return res
+
+        
