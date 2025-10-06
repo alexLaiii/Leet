94c8ca05e3713a11c0fb37ae74c85f820299547f
Alex Lai <127812749+alexLaiii@users.noreply.github.com>
2025-10-05 23:52:59 -0400

    Update 986. Interval List Intersections.py

--- medium/array sorting/986. Interval List Intersections.py
+++ medium/array sorting/986. Interval List Intersections.py
@@ -64,5 +64,27 @@ class Solution:
                     prevMerge = nextCheck
         return intersect
 
+"""
+Cleaner version
+"""
+
+class Solution:
+    def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
+        intersect = []
+        i , j = 0,0
+        mergedEnd = 0
+        while i < len(firstList) and j < len(secondList):
+            start = max(firstList[i][0], secondList[j][0])
+            end = min(firstList[i][1], secondList[j][1])
+            if end >= start:
+                intersect.append([start,end])
+            if firstList[i][1] > secondList[j][1]:
+                j += 1
+            else:
+                i += 1
+                
+        return intersect
+
+
                 
             
