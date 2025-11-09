1f1a8279835f98d7002da22bcbf4a262d059a7e7
Alex Lai <127812749+alexLaiii@users.noreply.github.com>
2025-11-09 01:19:55 -0500

    Add Solution class with countOperations method
    Implement countOperations method to count operations to zero.

---
+++ Easy/Math/2169. Count Operations to Obtain Zero.py
@@ -0,0 +1,15 @@
+"""
+Too easy, the problem description itself is the solution
+"""
+
+class Solution:
+    def countOperations(self, num1: int, num2: int) -> int:
+        k = 0
+        while num1 != 0 and num2 != 0:
+            if num1 >= num2:
+                num1 -= num2
+            else:
+                num2 -= num1
+            k += 1
+        return k
+            
