f6df857385f0e7e32d417ac34e4ddaeec1481956
Alex Lai <127812749+alexLaiii@users.noreply.github.com>
2026-06-29 01:49:11 -0400

    Add solution for counting substring patterns
    Implement a solution to count substrings in a word.

---
+++ Easy/String/1967. Number of Strings That Appear as Substrings in Word.py
@@ -0,0 +1,10 @@
+"""
+Very easy problem
+"""
+class Solution:
+    def numOfStrings(self, patterns: List[str], word: str) -> int:
+        res = 0
+        for w in patterns:
+            if w in word:
+                res += 1
+        return res
