b2bbfb2f699df7ec0a56bb0526b44097711a0625
Alex Lai <127812749+alexLaiii@users.noreply.github.com>
2026-09-05 21:12:53 -0400

    Create 2057. Smallest Index With Equal Value.py
    Add a new Python file for the 'Smallest Index With Equal Value' problem.

---
+++ Easy/Math/2057. Smallest Index With Equal Value.py
@@ -0,0 +1,9 @@
+"""
+Very simple problem
+"""
+class Solution:
+    def smallestEqual(self, nums: List[int]) -> int:
+        for i in range(len(nums)):
+            if i % 10 == nums[i]:
+                return i
+        return -1
